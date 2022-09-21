febb9f7b87035e714372f9b8dfded7858084b6c7
Jahanvi220104 <jahanvij22@gmail.com>
2022-09-21 17:28:36 -0400

    lab data

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const readFile = require("./readFile")
 
-readFile.myreadFile(".data/albums.json").then((albums)){
+readFile.myreadFile(".data/albums.json").then((albums) => {
 console.log(data)
-}.catch(err) =>{
-    console.log("ERROR: "+ err)
-}
+}).catch( (err) => {
+    console.log("ERROR: \n"+ err)
+})
